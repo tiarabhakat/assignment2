4d1b57e9e1cd65e789624799a98bc6bee21dadbc
tiarabhakat <tiara.bhakat@gmail.com>
2026-07-22 09:44:00 -0400

    React set up + code for StudentForm

---
+++ App/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+import StudentForm from StudentForm
+import StudentList from StudentList
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ App/src/StudentForm.jsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+
+export default function StudentForm(){
+    // registration states
+    const [first_name, setFirstName] = useState("");
+    const [last_name, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [program, setProgram] = useState("");
+
+    // error checking states
+    const [registered, setRegistered] = useState(false);
+    const [error, setError] = useState(false);
+
+    // handle changes - first_name, last_name, email, program
+    // Handling the name change
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+        setRegistered(false);
+    };
+
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+        setRegistered(false);
+    };
+
+    // Handling the email change
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        setRegistered(false);
+    };
+
+    // Handling the program change
+    const handleProgram = (e) => {
+        setProgram(e.target.value);
+        setRegistered(false);
+    };
+
+    // Handling the student registration
+    const handleRegistered = (e) => {
+        e.preventDefault();
+        if (first_name === "" ||last_name === "" || email === "" || program === "") {
+            setError(true);
+        } else {
+            setRegistered(true);
+            setError(false);
+        }
+    };
+
+    // Showing success message
+    const successMessage = () => {
+        return (
+            <div
+                className="success"
+                style={{
+                    display: submitted ? "" : "none",
+                }}
+            >
+                <h1>User {name} has been successfully registered!!</h1>
+            </div>
+        );
+    };
+
+    // Showing error message if error is true
+    const errorMessage = () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: error ? "" : "none",
+                }}
+            >
+                <h1>Please enter all the fields</h1>
+            </div>
+        );
+    };
+
+    return (
+        <div className="form">
+            <div>
+                <h1>Student Registration</h1>
+            </div>
+
+            {/* Calling to the methods */}
+            <div className="messages">
+                {errorMessage()}
+                {successMessage()}
+            </div>
+
+            <form>
+                {/* Labels and inputs for form data */}
+                <label className="label">First Name</label>
+                <input
+                    onChange={handleFirstName}
+                    className="input"
+                    value={first_name}
+                    type="text"
+                />
+
+                <label className="label">Last Name</label>
+                <input
+                    onChange={handleLastName}
+                    className="input"
+                    value={last_name}
+                    type="text"
+                />
+
+                <label className="label">Email</label>
+                <input
+                    onChange={handleEmail}
+                    className="input"
+                    value={email}
+                    type="email"
+                />
+
+                <label className="label">Program</label>
+                <input
+                    onChange={handleProgram}
+                    className="input"
+                    value={program}
+                    type="password"
+                />
+
+                <button onClick={handleRegistered} className="btn" type="register">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+}
